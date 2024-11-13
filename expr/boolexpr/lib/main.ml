46981129decc22520f32f881f22d822b9894cf0e
Docker <docker@example.com>
2024-11-13 15:40:13 +0000

    solved boolexpr

--- expr/boolexpr/lib/main.ml
+++ expr/boolexpr/lib/main.ml
@@ -14,10 +14,15 @@ let parse (s : string) : boolExpr =
 
 exception NoRuleApplies
 
+let rec eval = function
+    True -> true
+  | False -> false
+  | If(e0, e1, e2) -> if eval e0 then eval e1 else eval e2
+
 let rec trace1 = function
     If(True,e1,_) -> e1
   | If(False,_,e2) -> e2
-  | If(_,_,_) -> failwith "TODO"
+  | If(e0, e1, e2) -> If(trace1 e0, e1, e2)
   | _ -> raise NoRuleApplies
 
 let rec trace e = try
@@ -25,8 +30,3 @@ let rec trace e = try
     in e::(trace e')
   with NoRuleApplies -> [e]
 
-
-let rec eval = function
-    True -> true
-  | False -> false
-  | If(_,_,_) -> failwith "TODO"
--- expr/boolexpr/test/boolexpr.ml
+++ expr/boolexpr/test/boolexpr.ml
@@ -3,10 +3,28 @@ open BoolexprLib.Main
 let test_eval expr exp_result =
   (expr |> parse |> eval) = exp_result
 
+let test_trace1 expr =
+  (expr |> parse |> trace1)
+
 (* ### Unit tests for task 4 *)
 
-let%test "test_eval_1" = failwith "TODO"
+let%test "test_eval_1" = test_eval "false" false
+let%test "test_eval_2" = test_eval "true" true
+let%test "test_eval_3" = test_eval "if true then false else true" false
+let%test "test_eval_4" = test_eval "if false then false else true" true
+let%test "test_eval_5" = test_eval "if true then (if true then false else true) else (if true then true else false)" false
+let%test "test_eval_6" = test_eval "if (if false then false else false) then (if false then true else false) else (if true then false else true)" false
+let%test "test_eval_7" = test_eval "if (if (if false then false else false) then (if false then true else false) else (if true then false else true)) then (if false then true else false) else (if true then false else true)" false
 
 (* ### Unit tests for task 5 *)
 
-let%test "test_trace1_1" = failwith "TODO"
+let%test "test_trace1_1" = try let _ = test_trace1 "if true then true else false" in true with | _ -> false
+let%test "test_trace1_2" = try let _ = test_trace1 "if false then true else false" in true with | _ -> false
+let%test "test_trace1_3" = try let _ = test_trace1 "if false then false else false" in true with | _ -> false
+
+let%test "test_trace1_4" = try let _ = test_trace1 "false" in false with | _ -> true
+let%test "test_trace1_5" = try let _ = test_trace1 "true" in false with | _ -> true
+
+let%test "test_trace1_6" = List.length (trace (parse "if (if false then false else false) then (if false then true else false) else (if true then false else true)")) < 10
+
+
